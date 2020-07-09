f81adc8583d7f1c3847422cde13d983f2b0cf7d1
Eutiquio Javier Ortega Flores <javicrzx723@gmail.com>
2020-07-09 16:05:23 -0700

    Add Docs

---
+++ docs/precache-manifest.66b7e1f95c50d64e72bbb94a4ed13b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c029825d4276b8c6b69d7392c0e3eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87ccb450489954c30122",
+    "url": "/static/css/main.77d661ae.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ccb450489954c30122",
+    "url": "/static/js/main.98c03ce8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
